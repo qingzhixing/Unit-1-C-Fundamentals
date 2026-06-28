79760cb72d0de5edeabd13eb6c67ea5bccd50aa6
qingzhixing <qzsqqmail@qq.com>
2026-06-29 02:29:50 +0800

    📦 NEW: 23_gomoku_game,fix 23 execises.cases

--- exercises/23_gomoku_game/gomoku.c
+++ exercises/23_gomoku_game/gomoku.c
@@ -23,47 +23,68 @@
 
 static char board[SIZE][SIZE];
 
-void init_board(void)  { memset(board, '.', sizeof(board)); }
+void init_board(void) { memset(board, '.', sizeof(board)); }
 
 int onboard(int r, int c) { return r >= 0 && r < SIZE && c >= 0 && c < SIZE; }
-int empty(int r, int c)   { return board[r][c] == '.'; }
+int empty(int r, int c) { return board[r][c] == '.'; }
 
-int check_dir(int r, int c, int dr, int dc, char player)
-{
-	int count = 0;
-	for (int k = 0; k < 5; k++) {
-		int nr = r + k * dr, nc = c + k * dc;
-		if (nr < 0 || nr >= SIZE || nc < 0 || nc >= SIZE) return 0;
-		if (board[nr][nc] == player) count++; else break;
-	}
-	return count >= 5;
+int check_dir(int r, int c, int dr, int dc, char player) {
+    int count = 0;
+    for (int k = 0; k < 5; k++) {
+        int nr = r + k * dr, nc = c + k * dc;
+        if (nr < 0 || nr >= SIZE || nc < 0 || nc >= SIZE) return 0;
+        if (board[nr][nc] == player)
+            count++;
+        else
+            break;
+    }
+    return count >= 5;
 }
 
-int check_win(char player)
-{
-	int dirs[][2] = {{0,1},{1,0},{1,1},{1,-1}};
-	for (int i = 0; i < SIZE; i++)
-		for (int j = 0; j < SIZE; j++) {
-			if (board[i][j] != player) continue;
-			for (int d = 0; d < 4; d++)
-				if (check_dir(i, j, dirs[d][0], dirs[d][1], player)) return 1;
-		}
-	return 0;
+int check_win(char player) {
+    int dirs[][2] = {{0, 1}, {1, 0}, {1, 1}, {1, -1}};
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] != player) continue;
+            for (int d = 0; d < 4; d++)
+                if (check_dir(i, j, dirs[d][0], dirs[d][1], player)) return 1;
+        }
+    return 0;
 }
 
-int board_full(void)
-{
-	for (int i = 0; i < SIZE; i++)
-		for (int j = 0; j < SIZE; j++)
-			if (board[i][j] == '.') return 0;
-	return 1;
+int board_full(void) {
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            if (board[i][j] == '.') return 0;
+    return 1;
 }
 
-int main(void)
-{
-	init_board();
+int main(void) {
+    init_board();
 
-#error TODO: Implement game loop: alternate players, place, check win/draw. Run "clings hint" for help.
+    int turn = 0;
+    // TODO: Implement game loop: alternate players, place, check win/draw.
 
-	return 0;
+    while (1) {
+        char cur = (turn % 2 == 0) ? 'X' : 'O';
+        int r, c;
+        scanf("%d %d", &r, &c);
+        if (!onboard(r, c) || !empty(r, c)) {
+            printf("Invalid move. Try again.\n");
+            continue;
+        }
+        board[r][c] = cur;
+
+        if (check_win(cur)) {
+            printf("Player %c wins!\n", cur);
+            return 0;
+        }
+        if (board_full()) {
+            printf("Draw!\n");
+            return 0;
+        }
+        turn++;
+    }
+
+    return 0;
 }
