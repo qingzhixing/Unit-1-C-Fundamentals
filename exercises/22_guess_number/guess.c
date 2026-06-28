84840ddfa5f95275c81f1b4fffe41f8c378cdf23
qingzhixing <qzsqqmail@qq.com>
2026-06-29 02:13:26 +0800

    📦 NEW: 22_guess_number

--- exercises/22_guess_number/guess.c
+++ exercises/22_guess_number/guess.c
@@ -20,44 +20,77 @@
 
 /* 平台无关的伪随机数生成器 (Linear Congruential Generator) */
 static unsigned int _seed = 42;
-int my_rand(void) { _seed = _seed * 1103515245 + 12345; return (_seed >> 16) & 0x7fff; }
-
-static void generate_secret(int secret[4])
-{
-	int used[10] = {0};
-	for (int i = 0; i < 4; i++) {
-		int d;
-		do { d = my_rand() % 10; } while (used[d]);
-		secret[i] = d;
-		used[d] = 1;
-	}
+int my_rand(void) {
+    _seed = _seed * 1103515245 + 12345;
+    return (_seed >> 16) & 0x7fff;
 }
 
-static void check(const int secret[4], const int guess[4], int *a, int *b)
-{
-	*a = *b = 0;
-	for (int i = 0; i < 4; i++) {
-		if (guess[i] == secret[i]) {
-			(*a)++;
-		} else {
-			for (int j = 0; j < 4; j++) {
-				if (guess[i] == secret[j]) {
-					(*b)++;
-					break;
-				}
-			}
-		}
-	}
+static void generate_secret(int secret[4]) {
+    int used[10] = {0};
+    for (int i = 0; i < 4; i++) {
+        int d;
+        do {
+            d = my_rand() % 10;
+        } while (used[d]);
+        secret[i] = d;
+        used[d] = 1;
+    }
 }
 
-static int parse_guess(const char *s, int guess[4])
-{
-#error TODO: Parse guess string: check length, convert chars to digits. Run "clings hint" for help.
+static void check(const int secret[4], const int guess[4], int *a, int *b) {
+    *a = *b = 0;
+    for (int i = 0; i < 4; i++) {
+        if (guess[i] == secret[i]) {
+            (*a)++;
+        } else {
+            for (int j = 0; j < 4; j++) {
+                if (guess[i] == secret[j]) {
+                    (*b)++;
+                    break;
+                }
+            }
+        }
+    }
 }
 
-int main(void)
-{
-#error TODO: Implement game loop: generate, read guesses, check, print results. Run "clings hint" for help.
+static int parse_guess(const char *s, int guess[4]) {
+    int len = strlen(s);
+    if (len < 4) return 0;
+    for (int i = 0; i < 4; i++) {
+        if (s[i] < '0' || s[i] > '9') return 0;
+        guess[i] = s[i] - '0';
+    }
+}
 
-	return 0;
+int main(void) {
+    // TODO: Implement game loop: generate, read guesses, check, print results.
+    int secret[4] = {0};
+    int guess[4] = {0};
+    char input[10];
+    int guess_time = 0;
+
+    generate_secret(secret);
+
+    while (1) {
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;  // 输入结束，退出循环
+        }
+
+        guess_time++;
+
+        parse_guess(input, guess);
+
+        int a, b;
+
+        check(secret, guess, &a, &b);
+
+        printf("%dA%dB\n", a, b);
+
+        if (a == 4) {
+            printf("Congratulations! You got it in %d attempts!\n", guess_time);
+            break;
+        }
+    }
+    printf("The secret was: %d%d%d%d\n", secret[0], secret[1], secret[2], secret[3]);
+    return 0;
 }
