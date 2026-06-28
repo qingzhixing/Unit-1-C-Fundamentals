349e9132b5e2cc1f626da7e796a139e68cacc726
qingzhixing <qzsqqmail@qq.com>
2026-06-28 20:28:01 +0800

    📦 NEW: 06_multiplication_table

--- exercises/06_multiplication_table/table99.c
+++ exercises/06_multiplication_table/table99.c
@@ -16,12 +16,16 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-	int i = 0;
-	int j = 0;
+int main(void) {
+    int i = 0;
+    int j = 0;
 
-#error TODO: Nested for loops for multiplication table. Run "clings hint" for help.
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d*%d=%d\t", j, i, i * j);
+        }
+        printf("\n");
+    }
 
-	return 0;
+    return 0;
 }
