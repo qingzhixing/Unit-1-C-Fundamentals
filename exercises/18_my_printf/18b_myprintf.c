88598f672844b391d3c21a57fbc7ccc397d15d3e
qingzhixing <qzsqqmail@qq.com>
2026-06-29 01:02:39 +0800

    📦 NEW: 18b_myprintf

--- exercises/18_my_printf/18b_myprintf.c
+++ exercises/18_my_printf/18b_myprintf.c
@@ -17,48 +17,78 @@
  * 验证："a = 100, b = 0xC8\nc = A, s = helloworld\n"
  */
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
-void itoa(int num, char *buf, int base)
-{
-	char *hex = "0123456789ABCDEF";
-	int i = 0;
-	int j;
+void itoa(int num, char *buf, int base) {
+    char *hex = "0123456789ABCDEF";
+    int i = 0;
+    int j;
 
-	do
-	{
-		int rest = num % base;
-		buf[i++] = hex[rest];
-		num /= base;
-	} while (num != 0);
+    do {
+        int rest = num % base;
+        buf[i++] = hex[rest];
+        num /= base;
+    } while (num != 0);
 
-	buf[i] = '\0';
+    buf[i] = '\0';
 
-	for (j = 0; j < i / 2; j++)
-	{
-		char tmp = buf[j];
-		buf[j] = buf[i - 1 - j];
-		buf[i - 1 - j] = tmp;
-	}
+    for (j = 0; j < i / 2; j++) {
+        char tmp = buf[j];
+        buf[j] = buf[i - 1 - j];
+        buf[i - 1 - j] = tmp;
+    }
 }
 
-void myputs(char *buf)
-{
-	while (*buf)
-		putchar(*buf++);
+void myputs(char *buf) {
+    while (*buf) putchar(*buf++);
 }
 
-int myprintf(const char *format, ...)
-{
-#error TODO: Fix this exercise. Run "clings hint" for help.
-	return 0;
+int myprintf(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+
+    char c;
+    while ((c = *format++) != '\0') {
+        if (c != '%') {
+            putchar(c);
+            continue;
+        }
+        c = *format++;
+        switch (c) {
+            case 'd': {
+                char buf[64];
+                int num = va_arg(ap, int);
+                itoa(num, buf, 10);
+                myputs(buf);
+                break;
+            }
+            case 'x': {
+                char buf[64];
+                int num = va_arg(ap, int);
+                itoa(num, buf, 16);
+                myputs(buf);
+                break;
+            }
+            case 's': {
+                myputs(va_arg(ap, char *));
+                break;
+            }
+            case 'c': {
+                putchar(va_arg(ap, int));
+                break;
+            }
+        }
+    }
+
+    va_end(ap);
+
+    return 0;
 }
 
-int main(void)
-{
-	myprintf("a = %d, b = 0x%x\n", 100, 200);
-	myprintf("c = %c, s = %s\n", 'A', "helloworld");
+int main(void) {
+    myprintf("a = %d, b = 0x%x\n", 100, 200);
+    myprintf("c = %c, s = %s\n", 'A', "helloworld");
 
-	return 0;
+    return 0;
 }
